83fa04e9aa815a90c90089755f5ac03061e325d1
Guilherme Filheiro <guilhermefilheiro@gmail.com>
2024-03-05 10:11:42 -0300

    Iniciado a adição do sistema de interação

---
+++ Assets/Scripts/Interaction/Interactor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+interface IInteractable
+{
+    public void Interact();
+}
+public class Interactor : MonoBehaviour
+{
+    public Transform interactorSource;
+    public float interactRange;
+
+    public PlayerInput playerInput;
+    public InputAction interactAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactAction = playerInput.actions.FindAction("interact");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(interactAction.IsPressed())
+        {
+            Ray r = new Ray(interactorSource.position, interactorSource.forward);
+            if(Physics.Raycast(r, out RaycastHit hitInfo, interactRange))
+            {
+                if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+                {
+                    interactObj.Interact();
+                }
+            }
+        }
+    }
+}
